d96ea9bdeb9a8311a2c25080ab0e2dae556481d0
Gregory Bleiker <gregory.bleiker@bbv.ch>
2019-10-30 09:57:55 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# PATH environment cleaner for windows
+This script removes paths that are duplicated inside the System PATH and also removes duplicates that exist in the System and the User path at the same time. Use with *caution*, it will rewrite your paths.
---
+++ dup_remover.ps1
@@ -0,0 +1,69 @@
+﻿$RegKeySystem = ([Microsoft.Win32.Registry]::LocalMachine).OpenSubKey("SYSTEM\CurrentControlSet\Control\Session Manager\Environment", $True) 
+$RegKeyUser = ([Microsoft.Win32.Registry]::CurrentUser).OpenSubKey("Environment", $True) 
+$PathSystemValue = $RegKeySystem.GetValue("Path", $Null) 
+$PathUserValue = $RegKeyUser.GetValue("Path", $Null) 
+
+#Write-host "Original path :" + $PathValue  
+$PathUserValues = $PathUserValue.Split(";", [System.StringSplitOptions]::RemoveEmptyEntries)
+$PathSystemValues = $PathSystemValue.Split(";", [System.StringSplitOptions]::RemoveEmptyEntries) 
+
+$IsDuplicate = $False 
+$NewValues = @() 
+
+
+# first remove dups from system path
+ForEach ($Value in $PathSystemValues) 
+{ 
+    if ($NewValues -notcontains $Value) 
+    { 
+        $NewValues += $Value 
+    } 
+    else 
+    { 
+        $IsDuplicate = $True 
+        Write-Host "Duplicate System Path Entries found, removing" $Value 
+
+    } 
+} 
+
+if ($IsDuplicate) 
+{ 
+    $NewValue = $NewValues -join ";" 
+    $RegKeySystem.SetValue("Path", $NewValue, [Microsoft.Win32.RegistryValueKind]::ExpandString) 
+} 
+else 
+{ 
+    Write-Host "No duplicate System PATH entries found" 
+} 
+
+
+#remove duplicates in User Path
+$IsDuplicate = $False 
+$NewValues = @() 
+ForEach ($Value in $PathUserValues) 
+{ 
+    if ($PathSystemValues -notcontains $Value) 
+    { 
+        $NewValues += $Value 
+    } 
+    else 
+    { 
+        $IsDuplicate = $True 
+        Write-Host "Duplicate User Path Entries found, removing" $Value 
+    } 
+} 
+
+if ($IsDuplicate) 
+{ 
+    $NewValue = $NewValues -join ";" 
+    $RegKeyUser.SetValue("Path", $NewValue, [Microsoft.Win32.RegistryValueKind]::ExpandString) 
+} 
+else 
+{ 
+    Write-Host "No duplicate User PATH entries found" 
+} 
+
+
+
+$RegKeySystem.Close()
+$RegKeyUser.Close() 
