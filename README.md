9ca9ea50688752a23d0052b0a8f4b24f37e40029
Gregory Bleiker <gregory.bleiker@bbv.ch>
2020-01-09 16:16:03 +0100

    added backslash removal, test if path exits

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # PATH environment cleaner for windows
-This script removes paths that are duplicated inside the System PATH and also removes duplicates that exist in the System and the User path at the same time. Use with *caution*, it will rewrite your paths.
+
+This script removes paths that are duplicated inside the System PATH and also removes duplicates that exist in the System and the User path at the same time. Use with _caution_, it will rewrite your paths.
+
+The script does:
+
+- strips off all backslashes in the path
+- removes duplicate entries in user and system path
+- removes entries where the path does not exist
+- remove entries in user path if they exist in system path
---
+++ clean_path.ps1
@@ -0,0 +1,72 @@
+﻿$RegKeySystem = ([Microsoft.Win32.Registry]::LocalMachine).OpenSubKey("SYSTEM\CurrentControlSet\Control\Session Manager\Environment", $True) 
+$RegKeyUser = ([Microsoft.Win32.Registry]::CurrentUser).OpenSubKey("Environment", $True) 
+$PathSystemValue = $RegKeySystem.GetValue("Path", $Null) 
+$PathUserValue = $RegKeyUser.GetValue("Path", $Null) 
+
+#Write-host "Original path :" + $PathValue  
+$PathUserValues = $PathUserValue.Split(";", [System.StringSplitOptions]::RemoveEmptyEntries)
+$PathSystemValues = $PathSystemValue.Split(";", [System.StringSplitOptions]::RemoveEmptyEntries) 
+
+$HasDuplicatesOrInvalid = $False 
+$NewValues = @() 
+
+
+# first remove dups from system path
+ForEach ($Value in $PathSystemValues) { 
+    $strippedValue = $Value.trimEnd("\")
+    if ($NewValues -notcontains $strippedValue) { 
+        if (Test-Path -Path $strippedValue) {
+            $NewValues += $strippedValue 
+        }
+        else {
+            $HasDuplicatesOrInvalid = $True 
+            Write-Host "Invalid System Path Entries found, removing" $strippedValue 
+        }
+    } 
+    else { 
+        $HasDuplicatesOrInvalid = $True 
+        Write-Host "Duplicate System Path Entries found, removing" $strippedValue 
+    } 
+} 
+
+if ($HasDuplicatesOrInvalid) { 
+    $NewValue = $NewValues -join ";" 
+    $RegKeySystem.SetValue("Path", $NewValue, [Microsoft.Win32.RegistryValueKind]::ExpandString) 
+} 
+else { 
+    Write-Host "No duplicate System PATH entries found" 
+} 
+
+
+#remove duplicates in User Path
+$HasDuplicatesOrInvalid = $False 
+$NewValues = @() 
+ForEach ($Value in $PathUserValues) { 
+    $strippedValue = $Value.trimEnd("\")
+    if ($PathSystemValues -notcontains $strippedValue) {
+        if (Test-Path -Path $strippedValue) { 
+            $NewValues += $strippedValue 
+        }
+        else {
+            $HasDuplicatesOrInvalid = $True 
+            Write-Host "Invalid System Path Entries found, removing" $strippedValue 
+        } 
+    }
+    else { 
+        $HasDuplicatesOrInvalid = $True 
+        Write-Host "Duplicate User Path Entries found, removing" $strippedValue 
+    } 
+} 
+
+if ($HasDuplicatesOrInvalid) { 
+    $NewValue = $NewValues -join ";" 
+    $RegKeyUser.SetValue("Path", $NewValue, [Microsoft.Win32.RegistryValueKind]::ExpandString) 
+} 
+else { 
+    Write-Host "No duplicate User PATH entries found" 
+} 
+
+
+
+$RegKeySystem.Close()
+$RegKeyUser.Close() 
